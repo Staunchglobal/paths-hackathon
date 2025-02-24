e2beaf389609e8b47cba0f85c757a15f7f0ddcb0
AbubakarNaveed <abubakarnaveedminhas@gmail.com>
2025-02-24 14:54:40 +0500

    intial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          50: "#F7F7F7",
+          100: "#E5E5EA",
+          200: "#A8A8B3",
+          300: "#808080",
+          400: "#666666",
+          500: "#4D4D4D",
+          600: "#333333",
+          700: "#242424",
+          800: "#181818",
+          900: "#000000",
+        },
       },
     },
   },
